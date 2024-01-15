e2ef0e217f7bba0cc736900db6e4e897262c04fa
snavgar217 <snavgar217@g.educaand.es>
2024-01-15 09:24:44 +0100

    PUNTO DE INICIO

---
+++ src/main/java/daw/tarea5csamuelnavarro/Tarea5cSamuelNavarro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw.tarea5csamuelnavarro;
+
+/**
+ *
+ * @author snavgar
+ */
+public class Tarea5cSamuelNavarro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
